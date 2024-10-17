3bb7a6da39eafebe43a7233da557cd91fc7afe9e
Stefan Maroń <stefanmaron@outlook.de>
2024-10-17 06:19:37 +0200

    Only load the last 2 days of artifacts to speed up the pipeline a bit

--- scripts/Auto_load_versions.ps1
+++ scripts/Auto_load_versions.ps1
@@ -5,8 +5,8 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 [System.Collections.ArrayList]$Versions = @()
-# Get-BCArtifactUrl -select All -Type OnPrem -country $country -after ([DateTime]::Today.AddDays(-1)) | % {
-Get-BCArtifactUrl -select All -Type OnPrem -country $country | % {
+Get-BCArtifactUrl -select All -Type OnPrem -country $country -after ([DateTime]::Today.AddDays(-2)) | % {
+# Get-BCArtifactUrl -select All -Type OnPrem -country $country | % {
     [System.Uri]$Url = $_
     $TempString = $Url.AbsolutePath
     [version]$Version = $TempString.Split('/')[2]
